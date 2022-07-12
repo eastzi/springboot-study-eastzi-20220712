1ace43de06a43500606a626547c86f8521ab03dc
eastzi <zero7038@naver.com>
2022-07-12 21:02:09 +0900

    init 커밋

---
+++ src/main/java/com/springboot/studyeastzi/StudyEastziApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyeastzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEastziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyEastziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyeastzi/StudyEastziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyeastzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyEastziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
